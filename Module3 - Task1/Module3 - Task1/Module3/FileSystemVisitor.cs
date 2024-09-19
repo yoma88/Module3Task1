fd5bd958f4f271c6ec5f41794819ab75cd04cf72
yoma88 <yoma88@users.noreply.github.com>
2024-09-19 21:06:47 +0200

    Add files via upload

---
+++ Module3 - Task1/Module3 - Task1/Module3/FileSystemVisitor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3
+{
+    public class FileSystemVisitor
+    {
+        private readonly string _startingPath;
+        private readonly Func<string, bool> _filter;
+
+        public FileSystemVisitor(string startingPath) : this(startingPath, null)
+        {
+        }
+
+        public FileSystemVisitor(string startingPath, Func<string, bool> filter)
+        {
+            _startingPath = startingPath;
+            _filter = filter;
+        }
+
+        public IEnumerable<string> GetFiles()
+        {
+            if (!Directory.Exists(_startingPath))
+            {
+                Console.WriteLine("Directory doesn´t exist");
+                yield break;
+            }
+
+            foreach (var item in Traverse(_startingPath))
+            {
+                yield return item;
+            }
+        }
+
+        private IEnumerable<string> Traverse(string path)
+        {
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                if (_filter == null || _filter(directory))
+                {
+                    yield return directory; 
+                }
+
+                foreach (var file in Traverse(directory)) // 
+                {
+                    yield return file;
+                }
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                if (_filter == null || _filter(file))
+                {
+                    yield return file; 
+                }
+            }
+        }
+    }
+}
---
+++ Module3 - Task1/Module3 - Task1/Module3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var visitor = new FileSystemVisitor(@"C:\Users\Yohana_Espinoza\Desktop\cv",
+            path => Path.GetExtension(path) == ".doc"); // Change the directory path 
+
+            foreach (var file in visitor.GetFiles())
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
